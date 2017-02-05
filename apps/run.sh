e47be43bee196f8f861a646d5235d6ea3c8f046a
flochtililoch <flochtililoch@gmail.com>
2017-02-05 15:50:50 -0800

    Fix run command

--- apps/run.sh
+++ apps/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 APP=$(basename $1)
-MODE=${2:-up}
+MODE=${2:-up -d}
 SERVICE=${3:-}
 echo "Running app $APP"
 MAINTAINER=$(cat ../MAINTAINER) PLATFORM=$(uname -m) docker-compose -f $APP/docker-compose.yml $MODE $SERVICE
