398b177a340232b4e9332cb4df42620a1ba51d86
flochtililoch <flochtililoch@gmail.com>
2017-12-29 14:43:24 -0800

    Fix ci

--- ci.sh
+++ ci.sh
@@ -8,47 +8,54 @@ if [ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
   # get changed images
   IMAGES=$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep images/ | grep Dockerfile)
 
-  # for each image
-  cd images
-  for IMAGE in "${IMAGES[@]}"
-  do
-    IMAGE_PATH=$(echo $IMAGE | tr "/" "\n")
-    IMAGE_NAME=${IMAGE_PATH[1]}
-    PLATFORM=${IMAGE_PATH[2]}
+  if [ ! -z $IMAGES ]; then
+    # for each image
+    cd images
+    for IMAGE in "${IMAGES[@]}"
+    do
+      IMAGE_PATH=$(echo $IMAGE | tr "/" "\n")
+      IMAGE_NAME=${IMAGE_PATH[1]}
+      PLATFORM=${IMAGE_PATH[2]}
 
-    echo "Building $IMAGE_NAME for platform $PLATFORM"
-    ./build.sh $IMAGE_NAME $PLATFORM
+      echo "Building $IMAGE_NAME for platform $PLATFORM"
+      ./build.sh $IMAGE_NAME $PLATFORM
 
-    TAG=$(grep "ENV VERSION" ../$IMAGE | awk 'NF>1{print $NF}')
-    if [ ! -z $TAG ]; then
-      echo "Tagging $IMAGE_NAME for platform $PLATFORM with tag $TAG"
-      ./tag.sh $IMAGE_NAME $TAG $PLATFORM
+      TAG=$(grep "ENV VERSION" ../$IMAGE | awk 'NF>1{print $NF}')
+      if [ ! -z $TAG ]; then
+        echo "Tagging $IMAGE_NAME for platform $PLATFORM with tag $TAG"
+        ./tag.sh $IMAGE_NAME $TAG $PLATFORM
 
-      echo "Pushing $IMAGE_NAME for platform $PLATFORM with tag $TAG"
-      ./push.sh $IMAGE_NAME $TAG $PLATFORM
-    fi
-    echo "Tagging $IMAGE_NAME for platform $PLATFORM with tag latest"
-    ./tag.sh $IMAGE_NAME latest $PLATFORM
+        echo "Pushing $IMAGE_NAME for platform $PLATFORM with tag $TAG"
+        ./push.sh $IMAGE_NAME $TAG $PLATFORM
+      fi
+      echo "Tagging $IMAGE_NAME for platform $PLATFORM with tag latest"
+      ./tag.sh $IMAGE_NAME latest $PLATFORM
 
-    echo "Pushing $IMAGE_NAME for platform $PLATFORM with tag latest"
-    ./push.sh $IMAGE_NAME latest $PLATFORM
-  done
-  cd ..
+      echo "Pushing $IMAGE_NAME for platform $PLATFORM with tag latest"
+      ./push.sh $IMAGE_NAME latest $PLATFORM
+    done
+    cd ..
+
+  fi
 
   # get changed images references
-  IMAGES_REFS=$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep images/ | grep ref)
+  IMAGES=$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep images/ | grep ref)
 
-  # for each image reference
-  cd images
-  for IMAGE in "${IMAGES[@]}"
-  do
-    IMAGE_PATH=$(echo $IMAGE | tr "/" "\n")
-    IMAGE_NAME=${IMAGE_PATH[1]}
-    PLATFORM=${IMAGE_PATH[2]}
-    TAG=latest
-    echo "Cloning $IMAGE_NAME for platform $PLATFORM with tag $TAG"
-    ./clone.sh $IMAGE_NAME $TAG $PLATFORM
-  done
-  cd ..
+  if [ ! -z $IMAGES_REFS ]; then
+
+    # for each image reference
+    cd images
+    for IMAGE in "${IMAGES[@]}"
+    do
+      IMAGE_PATH=$(echo $IMAGE | tr "/" "\n")
+      IMAGE_NAME=${IMAGE_PATH[1]}
+      PLATFORM=${IMAGE_PATH[2]}
+      TAG=latest
+      echo "Cloning $IMAGE_NAME for platform $PLATFORM with tag $TAG"
+      ./clone.sh $IMAGE_NAME $TAG $PLATFORM
+    done
+    cd ..
+
+  fi
 
 fi
