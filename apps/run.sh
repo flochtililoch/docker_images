3a641c8d3c58ec0b82343a7ff1f2a3124c92ad2e
flochtililoch <flochtililoch@gmail.com>
2018-01-17 21:56:33 -0800

    Add pi-hole app

--- apps/run.sh
+++ apps/run.sh
@@ -3,4 +3,6 @@
 APP=$(basename $1)
 MODE=${2:-up -d}
 SERVICE=${3:-}
+HOST_IP=$(sudo ifconfig wlan0 | grep 'inet addr' | xargs | cut -d' ' -f2 | cut -d':' -f2)
+HOST_IP_V6=$(sudo ifconfig wlan0 | grep 'inet6 addr' | xargs | cut -d' ' -f3)
 MAINTAINER=$(cat ../MAINTAINER) PLATFORM=$(uname -m) docker-compose -f $APP/docker-compose.yml $MODE $SERVICE
