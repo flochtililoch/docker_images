7408b035efa040b9b8a8f6d1ff4358744b2ffbde
flochtililoch <flochtililoch@gmail.com>
2018-01-20 10:12:39 -0800

    Update run script

--- apps/run.sh
+++ apps/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 APP=$(basename $1)
-MODE=${2:-up -d}
-SERVICE=${3:-}
-HOST_IP=$(sudo ifconfig wlan0 | grep 'inet addr' | xargs | cut -d' ' -f2 | cut -d':' -f2)
 HOST_IP_V6=$(sudo ifconfig wlan0 | grep 'inet6 addr' | xargs | cut -d' ' -f3)
-MAINTAINER=$(cat ../MAINTAINER) PLATFORM=$(uname -m) docker-compose -f $APP/docker-compose.yml $MODE $SERVICE
+HOST_IP=$(sudo ifconfig wlan0 | grep 'inet addr' | xargs | cut -d' ' -f2 | cut -d':' -f2)
+MAINTAINER=$(cat ../MAINTAINER)
+MODE=${2:-up -d}
+PLATFORM=$(uname -m)
+SERVICE=${3:-}
+
+MAINTAINER=$MAINTAINER PLATFORM=$PLATFORM HOST_IP=$HOST_IP HOST_IP_V6=$HOST_IP_V6 docker-compose -f $APP/docker-compose.yml $MODE $SERVICE
