b41b0995be24d8323e7155383c8c2a09fefeafe0
flochtililoch <flochtililoch@gmail.com>
2018-05-12 14:19:09 -0700

    Re-enable ci on master

---
+++ build-commit-range.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+COMMIT_RANGE=$1
+IMAGES=$(git diff --name-only $COMMIT_RANGE | grep Dockerfile)
+
+if [ ! -z $IMAGES ]; then
+
+  echo "Building images"
+  echo "==============="
+
+  # for each image
+  for IMAGE in "${IMAGES[@]}"
+  do
+    IMAGE_PATH=$(echo $IMAGE | tr "/" "\n")
+    IMAGE_ARRAY=(${IMAGE_PATH//;/ })
+    IMAGE_NAME=${IMAGE_ARRAY[0]}
+    PLATFORM=${IMAGE_ARRAY[1]}
+
+    echo "Building $IMAGE_NAME for platform $PLATFORM"
+    ./build.sh $IMAGE_NAME $PLATFORM
+    
+    TAG=$(grep "ENV VERSION" $IMAGE | awk 'NF>1{print $NF}')
+    if [ ! -z $TAG ]; then
+      echo "Tagging $IMAGE_NAME for platform $PLATFORM with tag $TAG"
+      ./tag.sh $IMAGE_NAME $TAG $PLATFORM
+
+      echo "Pushing $IMAGE_NAME for platform $PLATFORM with tag $TAG"
+      ./push.sh $IMAGE_NAME $TAG $PLATFORM
+    fi
+    echo "Tagging $IMAGE_NAME for platform $PLATFORM with tag latest"
+    ./tag.sh $IMAGE_NAME latest $PLATFORM
+
+    echo "Pushing $IMAGE_NAME for platform $PLATFORM with tag latest"
+    ./push.sh $IMAGE_NAME latest $PLATFORM
+  done
+
+fi
+
+# get changed images references
+IMAGES=$(git diff --name-only $COMMIT_RANGE | grep ref)
+if [ ! -z $IMAGES ]; then
+
+  echo "Cloning images"
+  echo "==============="
+
+  # for each image reference
+  for IMAGE in "${IMAGES[@]}"
+  do
+    IMAGE_PATH=$(echo $IMAGE | tr "/" "\n")
+    IMAGE_ARRAY=(${IMAGE_PATH//;/ })
+    IMAGE_NAME=${IMAGE_ARRAY[0]}
+    PLATFORM=${IMAGE_ARRAY[1]}
+    TAG=latest
+    echo "Cloning $IMAGE_NAME for platform $PLATFORM with tag $TAG"
+    ./clone.sh $IMAGE_NAME $TAG $PLATFORM
+  done
+  cd ..
+
+fi
--- ci.sh
+++ ci.sh
@@ -1,66 +1,7 @@
 #!/bin/bash
 
-if [ $TRAVIS_BRANCH == "disabled" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
-  # prepare environment
+if [ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
   docker run --rm --privileged multiarch/qemu-user-static:register --reset
   docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
-
-  # get changed images
-  IMAGES=$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep Dockerfile)
-
-  if [ ! -z $IMAGES ]; then
-
-    echo "Building images"
-    echo "==============="
-
-    # for each image
-    for IMAGE in "${IMAGES[@]}"
-    do
-      IMAGE_PATH=$(echo $IMAGE | tr "/" "\n")
-      IMAGE_ARRAY=(${IMAGE_PATH//;/ })
-      IMAGE_NAME=${IMAGE_ARRAY[1]}
-      PLATFORM=${IMAGE_ARRAY[2]}
-
-      echo "Building $IMAGE_NAME for platform $PLATFORM"
-      ./build.sh $IMAGE_NAME $PLATFORM
-      
-      TAG=$(grep "ENV VERSION" ../$IMAGE | awk 'NF>1{print $NF}')
-      if [ ! -z $TAG ]; then
-        echo "Tagging $IMAGE_NAME for platform $PLATFORM with tag $TAG"
-        ./tag.sh $IMAGE_NAME $TAG $PLATFORM
-
-        echo "Pushing $IMAGE_NAME for platform $PLATFORM with tag $TAG"
-        ./push.sh $IMAGE_NAME $TAG $PLATFORM
-      fi
-      echo "Tagging $IMAGE_NAME for platform $PLATFORM with tag latest"
-      ./tag.sh $IMAGE_NAME latest $PLATFORM
-
-      echo "Pushing $IMAGE_NAME for platform $PLATFORM with tag latest"
-      ./push.sh $IMAGE_NAME latest $PLATFORM
-    done
-
-  fi
-
-  # get changed images references
-  IMAGES=$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep ref)
-  if [ ! -z $IMAGES ]; then
-
-    echo "Cloning images"
-    echo "==============="
-
-    # for each image reference
-    for IMAGE in "${IMAGES[@]}"
-    do
-      IMAGE_PATH=$(echo $IMAGE | tr "/" "\n")
-      IMAGE_ARRAY=(${IMAGE_PATH//;/ })
-      IMAGE_NAME=${IMAGE_ARRAY[1]}
-      PLATFORM=${IMAGE_ARRAY[2]}
-      TAG=latest
-      echo "Cloning $IMAGE_NAME for platform $PLATFORM with tag $TAG"
-      ./clone.sh $IMAGE_NAME $TAG $PLATFORM
-    done
-    cd ..
-
-  fi
-
+  ./build-commit-range.sh $TRAVIS_COMMIT_RANGE
 fi
